f44e85242bb2da0735bd6f37bcaef5cbceb3fa62
mcbalz <mcbalz@gmail.com>
2025-12-07 15:58:54 -0500

    Linted server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 '''Server for a flask app version of the Emotion detection project.'''
 
-from flask import Flask, request, url_for, redirect, jsonify, render_template
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
@@ -13,11 +13,16 @@ def index():
 def emotional():
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
-    
+
     if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    
-    response = f"""For the given statement the system response is: 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, and 'sadness': {result['sadness']}. The dominant emotion is <strong>{result['dominant_emotion']}</strong>."""
+
+    response = (
+        f"For the given statement the system response is: 'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, "
+        f"and 'sadness': {result['sadness']}. The dominant emotion is "
+        f"<strong>{result['dominant_emotion']}</strong>."
+    )
     return response
 
 if __name__ == "__main__":
