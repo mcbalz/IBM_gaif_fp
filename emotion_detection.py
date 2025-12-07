a76befe8e6f59a02040e48fa3bb5d50d8ebc1520
Matt Baldwin <64975429+mcbalz@users.noreply.github.com>
2025-12-07 12:02:40 -0500

    Add emotion detection function in emotion_detection.py
    Implemented the emotion detection function using Watson's NLP service.

---
+++ emotion_detection.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+'''In this file, we will write the function to run emotion detection.'''
+
+# TO DO: write function emotion_detector
+def emotion_detector(text_to_detect):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+      "grpc-metadata-mm-model-id": 
+      "emotion_aggregated-workflow_lang_en_stock"
+    }
+            
+    myobj = { "raw_document": { "text": text_for_analysis} }
+    response = requests.post(url, json=myobj, headers=headers)
+    # print(response.text)
+    # data = json.loads(response.text)
+    # final_result = {}
+    # final_result['score'] = data['documentSentiment']['score']
+    # final_result['label'] = data['documentSentiment']['label']
+    return response.text
+
+# TO DO: take a screenshot of the code written and save it as 2a_emotion_detection.png
+
+# TO DO: test out the function in python3, using "I love this new technology" string 
+
+# TO DO: screenshot the terminal with all three steps included in it including final output 
+# name the screenshot 2b_application_creation.png
+
